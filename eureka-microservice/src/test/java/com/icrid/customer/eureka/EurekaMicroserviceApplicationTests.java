5cf226c9f5699a6e3281216b797f91638ca22432
FrankieRM <frodriguezm17@gmail.com>
2019-12-20 04:26:12 -0500

    implementando eureka-microservice

---
+++ eureka-microservice/src/test/java/com/icrid/customer/eureka/EurekaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icrid.customer.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaMicroserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
